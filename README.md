761cbfeacda035c2813500aa743d745329dd4910
adinata15 <adinatatan99@gmail.com>
2024-05-25 14:08:30 +0800

    add https to prod url name

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Todo
 - Create devcontainer for the development (how to forward the port to local pc?)
-- Find a good way to import design from figma
--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			"@/*": "./src/lib/*",
 		},
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? 'adinata15.github.io/commando-web/' : '',
+            base: process.env.NODE_ENV === 'production' ? 'https://adinata15.github.io/commando-web/' : '',
         },
 	},
 	preprocess: vitePreprocess()
